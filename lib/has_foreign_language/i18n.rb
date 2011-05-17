0a15faf01e50de682d196b2e9da52f2632747e81
Andrea Dal Ponte <dalpo85@gmail.com>
2011-05-17 15:33:37 +0200

    Namespace fix

--- lib/has_foreign_language/i18n.rb
+++ lib/has_foreign_language/i18n.rb
@@ -1,45 +1,42 @@
 # HasForeignLanguage
 module HasForeignLanguage
-  
-  module I18n
-  
-    def self.included(mod)
-      mod.extend(ClassMethods)
-    end
 
-    module ClassMethods
-  
-      def has_foreign_language(*args)
-        args.each do |field|
-                  
-          # Define the marker
-          eigenclass = class << self
-            self
-          end
-          eigenclass.class_eval do
-            define_method("has_foreign_language_#{field.to_s}?") { true }            
-          end
+  def self.included(mod)
+    mod.extend(ClassMethods)
+  end
 
-          # Define the getter
-          define_method(field.to_s) do
-            if self.class.columns.select {|c| c.name == "#{field}_#{I18n.locale}"}.length > 0
-              self.send("#{field}_#{I18n.locale}".to_sym)
-            else
-              super()
-            end
-          end
+  module ClassMethods
 
-          # Define the setter
-          define_method("#{field}=") do |val|
-            if self.class.columns.select {|c| c.name == "#{field}_#{I18n.locale}"}.length > 0
-              self["#{field}_#{I18n.locale}".to_sym] = val
-            else
-              self[field.to_sym] = val
-            end
+    def has_foreign_language(*args)
+      args.each do |field|
+                
+        # Define the marker
+        eigenclass = class << self
+          self
+        end
+        eigenclass.class_eval do
+          define_method("has_foreign_language_#{field.to_s}?") { true }            
+        end
+
+        # Define the getter
+        define_method(field.to_s) do
+          if self.class.columns.select {|c| c.name == "#{field}_#{I18n.locale}"}.length > 0
+            self.send("#{field}_#{I18n.locale}".to_sym)
+          else
+            super()
           end
         end
+
+        # Define the setter
+        define_method("#{field}=") do |val|
+          if self.class.columns.select {|c| c.name == "#{field}_#{I18n.locale}"}.length > 0
+            self["#{field}_#{I18n.locale}".to_sym] = val
+          else
+            self[field.to_sym] = val
+          end
+        end
+        
       end
-      
     end
     
   end
--- lib/has_foreign_language/init.rb
+++ lib/has_foreign_language/init.rb
@@ -1,6 +1,6 @@
 # Include hook code here
 
-ActiveRecord::Base.send(:include, HasForeignLanguage::I18n)
+ActiveRecord::Base.send(:include, HasForeignLanguage)
 
 
 #ActionView::Helpers::FormHelper.send(:extend, FormFix)
